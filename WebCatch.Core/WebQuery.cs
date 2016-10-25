b5adb861558f9d8fc54727485d891b56d64bba8e
zouhua <eric.zh24@gmail.com>
2016-10-26 00:44:18 +0800

    dd

--- WebCatch.Core/WebQuery.cs
+++ WebCatch.Core/WebQuery.cs
@@ -71,7 +71,7 @@ namespace WebCatch.Core
         }
        
 
-        public async Task Exceute<TResult>(Func<Dictionary<string, IHtmlCollection<IElement>>, TResult> action)
+        public async Task Exceute<TResult>(Func<Dictionary<string, IEnumerable<string>>, TResult> action)
         {
             var httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.UserAgent.TryParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36");
@@ -79,83 +79,106 @@ namespace WebCatch.Core
             var config = Configuration.Default.WithDefaultLoader(requesters: new[] { requester });
             var context = BrowsingContext.New(config);
 
+            Dictionary<string, IEnumerable<string>> dicResult;
+
 
             if (subQuery != null)
             {
-                var rlist = new List<IHtmlAnchorElement>();
-
-                var document = subQuery.method == HttpMethod.Post ? await context.PostAsync(Url.Create(subQuery.url), subQuery.body, MimeTypeNames.UrlencodedForm, subQuery.url)
-                    : await context.OpenAsync(subQuery.url, subQuery.url);
-                foreach (var item in subQuery.QuerySelectors)
+                dicResult = await Exceute(context, subQuery);
+                if (dicResult != null)
                 {
-                    IEnumerable<IHtmlAnchorElement> anchors;
+                    var document = await context.OpenAsync(mx => mx.Address(subQuery.url).Content(string.Join("", dicResult.First().Value.Select(m => string.Format("<a href='{0}'></a>", m.ToString())))));
 
-                    if (subQuery.contextType == "json")
+                    var anchors = document.QuerySelectorAll<IHtmlAnchorElement>("a");
+
+                    foreach (var anchor in anchors)
                     {
-                        var jts = JObject.Parse(document.Body.TextContent).SelectTokens(item.Value);
-                        document = await context.OpenAsync(m => m.Address(url)
-                                                                    .Content(string.Join("",
-                                                                    jts.Select(j => string.Format("<a href='{0}'></a>", j.ToString())))));
-                        anchors = document.QuerySelectorAll<IHtmlAnchorElement>("a");
-                    }
-                    else
-                    {
-                         anchors = document.QuerySelectorAll<IHtmlAnchorElement>(item.Value);
-                    }
-
-                    if (anchors != null && anchors.Count() > 0)
-                    {
-                        foreach (var anchor in anchors)
-                        {
-                            var elements = new Dictionary<string, IHtmlCollection<IElement>>();
-
-                            var subDocument = await anchor.NavigateAsync();
-
-                            foreach (var curSel in this.QuerySelectors)
-                            {
-                                elements.Add(curSel.Key, subDocument.QuerySelectorAll(curSel.Value));
-                            }
-
-                            action(elements);
-                        }
+                        this.url = anchor.Href;
+                        dicResult = await Exceute(context, this);
+                        action(dicResult);
                     }
                 }
+
             }
+            else
+            {
+                dicResult = await Exceute(context, this);
+                action(dicResult);
+            }
+
+
+
+            //if (subQuery != null)
+            //{
+            //    var rlist = new List<IHtmlAnchorElement>();
+
+            //    var document = subQuery.method == HttpMethod.Post ? await context.PostAsync(Url.Create(subQuery.url), subQuery.body, MimeTypeNames.UrlencodedForm, subQuery.url)
+            //        : await context.OpenAsync(subQuery.url, subQuery.url);
+            //    foreach (var item in subQuery.QuerySelectors)
+            //    {
+            //        IEnumerable<IHtmlAnchorElement> anchors;
+
+            //        if (subQuery.contextType == "json")
+            //        {
+            //            var jts = JObject.Parse(document.Body.TextContent).SelectTokens(item.Value);
+            //            document = await context.OpenAsync(m => m.Address(url)
+            //                                                        .Content(string.Join("",
+            //                                                        jts.Select(j => string.Format("<a href='{0}'></a>", j.ToString())))));
+            //            anchors = document.QuerySelectorAll<IHtmlAnchorElement>("a");
+            //        }
+            //        else
+            //        {
+            //             anchors = document.QuerySelectorAll<IHtmlAnchorElement>(item.Value);
+            //        }
+
+            //        if (anchors != null && anchors.Count() > 0)
+            //        {
+            //            foreach (var anchor in anchors)
+            //            {
+            //                var elements = new Dictionary<string, IHtmlCollection<IElement>>();
+
+            //                var subDocument = await anchor.NavigateAsync();
+
+            //                foreach (var curSel in this.QuerySelectors)
+            //                {
+            //                    elements.Add(curSel.Key, subDocument.QuerySelectorAll(curSel.Value));
+            //                }
+
+            //                action(elements);
+            //            }
+            //        }
+            //    }
+            //}
         }
 
-        private async Task Exceute<T1, TResult>(IBrowsingContext context, WebQuery webQuery, Func<T1, TResult> action)
+        private async Task<Dictionary<string, IEnumerable<string>>> Exceute(IBrowsingContext context, WebQuery webQuery)
         {
 
             var document = webQuery.method == HttpMethod.Post ? await context.PostAsync(Url.Create(webQuery.url), webQuery.body, MimeTypeNames.UrlencodedForm, webQuery.url)
                    : await context.OpenAsync(webQuery.url, webQuery.url);
 
-            var elements = new Dictionary<string, IHtmlCollection<IElement>>();
+            var elements = new Dictionary<string, IEnumerable<string>>();
 
+            JObject job = null;
 
             if (webQuery.contextType == "json")
             {
-                var job = JObject.Parse(document.Source.Text);
-                foreach (var item in webQuery.QuerySelectors)
-                {
-                   // elements.Add(item.Key, job.SelectTokens(item.Value));
-                }
+                job = JObject.Parse(document.Body.TextContent);
             }
-
             foreach (var item in webQuery.QuerySelectors)
             {
-                if (webQuery.contextType == "json")
+                if (job != null)
                 {
-                    var jts = JObject.Parse(document.Body.TextContent).SelectTokens(item.Value);
-
+                    var jts = job.SelectTokens(item.Value);
+                    elements.Add(item.Key, jts.Select(m => m.ToString()));
                 }
                 else
                 {
-                    elements.Add(item.Key, document.QuerySelectorAll(item.Value));
+                    elements.Add(item.Key, document.QuerySelectorAll(item.Value).Select(m => m.OuterHtml));
                 }
             }
 
-           // action((T1)elements);
-
+            return elements;
         }
     }
 }
